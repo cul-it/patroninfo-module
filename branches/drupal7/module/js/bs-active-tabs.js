72e69790a1875c84912dd002f0474e9001a0b8d1
mhk33 <mhk33@d80fea99-f317-4d9d-8167-ad00dbd55a12>
2015-11-10 14:57:09 +0000

    jquery instead of $

--- branches/drupal7/module/js/bs-active-tabs.js
+++ branches/drupal7/module/js/bs-active-tabs.js
@@ -1,4 +1,4 @@
 jQuery(document).ready(function() {
-  $('.navtabs li:first-child').addClass('active');
-  $('#frag-0').addClass('active');
+  jQuery('.navtabs li:first-child').addClass('active');
+  jQuery('#frag-0').addClass('active');
 });
